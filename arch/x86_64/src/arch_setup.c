b42de2ecd614a9ce179e1c5de784d638e6ab9369
lemon <borbtrash@gmail.com>
2025-12-05 04:09:56 -0500

    beautify

--- arch/x86_64/src/arch_setup.c
+++ arch/x86_64/src/arch_setup.c
@@ -16,10 +16,10 @@ void arch_setup() {
 		return;
 	}
 	uint32_t oldflags = feature_flags;
-	int fd = open("/dev/mem", O_RDWR | O_SYNC);
-	uint64_t apic_address;
 	feature_flags |= TRAP_EMULATION;
-	apic_address = rdmsr(0x1b);
+
+	int fd = open("/dev/mem", O_RDWR | O_SYNC);
+	uint64_t apic_address = rdmsr(0x1b); // APIC_BASE_MSR
 	mmap((void *) (uintptr_t) (apic_address & ~0xfffllu), 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, apic_address & ~0xfffllu);
 	feature_flags = oldflags;
 }
